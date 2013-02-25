0b8faa3eb089f4661e70e8e6d72734a80c5541b4
Sylvain Deloux <sylvain.deloux@gmail.com>
2013-02-25 10:30:37 +0100

    Added faces

--- responders/rageface.py
+++ responders/rageface.py
@@ -5,10 +5,20 @@ class RagefaceResponder(Responder):
         "troll": "http://fuuu.us/86.png",
         "lol": "http://fuuu.us/176.png",
         "sir": "http://fuuu.us/389.png",
-        "fuuu": "http://fuuu.us/217.png",
+        "deskflip": "http://fuuu.us/217.png",
         "true": "http://fuuu.us/285.png",
         "cute": "http://fuuu.us/447.png",
         "win": "http://fuuu.us/188.png",
+        "notbad": "http://fuuu.us/172.png",
+        "fuckyeah": "http://fuuu.us/12.png",
+        "genius": "http://fuuu.us/292.png",
+        "pokerface": "http://fuuu.us/268.png",
+        "cerealguy": "http://fuuu.us/144.png",
+        "longneck": "http://fuuu.us/107.png",
+        "megusta": "http://fuuu.us/35.png",
+        "badumtss": "http://fuuu.us/363.png",
+        "rageguy": "http://fuuu.us/61.png",
+        "nothing": "http://fuuu.us/230.png",
     }
 
     def support(self, message):
