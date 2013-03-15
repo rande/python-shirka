61d13adecbdde70b3895e19382c3b7e12846c6a3
Thomas Rabaix <thomas.rabaix@ekino.com>
2013-03-15 02:06:06 +0100

    Made the rage face customizable

--- shirka/responders/rageface.py
+++ shirka/responders/rageface.py
@@ -3,27 +3,30 @@
 from shirka.responders import Responder
 from shirka.consumers import BaseTestCase
 
+DEFAULT_IMG = {
+    "badumtss": "http://fuuu.us/363.png",
+    "cerealguy": "http://fuuu.us/144.png",
+    "cute": "http://fuuu.us/447.png",
+    "deskflip": "http://fuuu.us/217.png",
+    "fuckyeah": "http://fuuu.us/12.png",
+    "genius": "http://fuuu.us/292.png",
+    "itssomething": "http://fuuu.us/408.png",
+    "lol": "http://fuuu.us/176.png",
+    "longneck": "http://fuuu.us/107.png",
+    "megusta": "http://fuuu.us/35.png",
+    "notbad": "http://fuuu.us/172.png",
+    "nothing": "http://fuuu.us/230.png",
+    "pokerface": "http://fuuu.us/268.png",
+    "rageguy": "http://fuuu.us/61.png",
+    "sir": "http://fuuu.us/389.png",
+    "troll": "http://fuuu.us/86.png",
+    "true": "http://fuuu.us/285.png",
+    "win": "http://fuuu.us/188.png",
+}
+
 class RagefaceResponder(Responder):
-    IMG = {
-        "badumtss": "http://fuuu.us/363.png",
-        "cerealguy": "http://fuuu.us/144.png",
-        "cute": "http://fuuu.us/447.png",
-        "deskflip": "http://fuuu.us/217.png",
-        "fuckyeah": "http://fuuu.us/12.png",
-        "genius": "http://fuuu.us/292.png",
-        "itssomething": "http://fuuu.us/408.png",
-        "lol": "http://fuuu.us/176.png",
-        "longneck": "http://fuuu.us/107.png",
-        "megusta": "http://fuuu.us/35.png",
-        "notbad": "http://fuuu.us/172.png",
-        "nothing": "http://fuuu.us/230.png",
-        "pokerface": "http://fuuu.us/268.png",
-        "rageguy": "http://fuuu.us/61.png",
-        "sir": "http://fuuu.us/389.png",
-        "troll": "http://fuuu.us/86.png",
-        "true": "http://fuuu.us/285.png",
-        "win": "http://fuuu.us/188.png",
-    }
+    def __init__(self, faces=DEFAULT_IMG):
+        self.faces = faces
 
     def name(self):
         return 'face'
@@ -39,12 +42,12 @@ class RagefaceResponder(Responder):
             return False
 
         if words[1] == 'help':
-            return "Face available: %s" % (", ".join(RagefaceResponder.IMG))
+            return "Face available: %s" % (", ".join(self.faces))
 
-        if words[1] in RagefaceResponder.IMG:
-            return RagefaceResponder.IMG[words[1]]
+        if words[1] in self.faces:
+            return self.faces[words[1]]
 
-        return False
+        return "You fail!! This face does not exist"
 
 
 class TestRagefaceResponder(BaseTestCase):
